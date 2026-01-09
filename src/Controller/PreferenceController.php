05e303a1e85cf6aa72e012e7459b1721b0c7a512
r3soares <r3soares@gmail.com>
2026-01-09 10:29:30 -0300

    Adicionado arquivos ausentes

---
+++ src/Controller/PreferenceController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use App\Service\GlobalVars;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class PreferenceController extends AbstractController
+{
+    function __construct(private readonly GlobalVars $globalVars)
+    {}
+
+    //#[Route('/profile/preference', name: 'app_preference')]
+    public function index(): Response
+    {
+        return $this->render('preference/index.html.twig', [
+            'controller_name' => 'PreferenceController',
+        ]);
+    }
+
+    #[Route(null, name: 'app_preference_change_theme')]
+    public function changeTheme(Request $request) : Response
+    {
+        $currentTheme = $this->globalVars->theme;
+        $this->globalVars->theme = ($currentTheme == 'light') ? 'dark' : 'light';
+        return $this->redirect($request->headers->get('referer') ?? $this->generateUrl('app_home'));
+    }
+}
